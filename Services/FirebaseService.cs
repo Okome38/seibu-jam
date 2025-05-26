2b60a6a15a1e2008223a8ae5ae0f8da2f908d221
Okome38 <ayato.momochan@gmail.com>
2025-05-26 14:47:31 +0900

    Create FirebaseService.cs

---
+++ Services/FirebaseService.cs
@@ -0,0 +1,32 @@
+using Firebase.Database;
+using Firebase.Database.Query;
+
+namespace seibuDatabase.Services
+{
+    public class FirebaseService
+    {
+        private readonly FirebaseClient _firebase;
+
+        public FirebaseService()
+        {
+            _firebase = new FirebaseClient("https://seibudatabase-default-rtdb.firebaseio.com/");
+        }
+
+        public async Task AddMessage(string name, string message)
+        {
+            await _firebase
+              .Child("messages")
+              .PostAsync(new { name = name, message = message, timestamp = DateTime.UtcNow });
+        }
+
+        public async Task<List<Message>> GetMessages()
+        {
+            var messages = await _firebase
+              .Child("messages")
+              .OrderBy("timestamp")
+              .OnceAsync<Message>();
+
+            return messages.Select(x => x.Object).ToList();
+        }
+    }
+}
