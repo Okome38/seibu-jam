591dd0c2aaaa7ddb5c75b8e8ed2df44529d0edbd
Okome38 <ayato.momochan@gmail.com>
2025-05-26 21:05:06 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,37 @@
+// var builder = WebApplication.CreateBuilder(args);
+
+// // Add services to the container.
+// builder.Services.AddControllersWithViews();
+
+// var app = builder.Build();
+
+// var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+// builder.WebHost.UseUrls($"http://*:{port}");
+
+// // Configure the HTTP request pipeline.
+// if (!app.Environment.IsDevelopment())
+// {
+//     app.UseExceptionHandler("/Home/Error");
+//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//     app.UseHsts();
+// }
+
+// app.UseHttpsRedirection();
+// app.UseRouting();
+
+// app.UseAuthorization();
+
+// //app.MapStaticAssets();
+
+// app.MapControllers(); 
+
+// app.MapControllerRoute(
+//     name: "default",
+//     pattern: "{controller=Home}/{action=Index}/{id?}");
+//     //.WithStaticAssets();
+
+
+// app.Run();
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -17,18 +51,15 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles(); // 静的ファイルの提供を有効化
 app.UseRouting();
 
 app.UseAuthorization();
-
-//app.MapStaticAssets();
 
 app.MapControllers(); 
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-    //.WithStaticAssets();
-
 
 app.Run();
