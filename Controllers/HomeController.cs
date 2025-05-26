538fc08d94a9b2ba8d56fa871a2f31ea6119853b
Okome38 <ayato.momochan@gmail.com>
2025-05-26 15:30:11 +0900

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,43 +3,16 @@ using Microsoft.AspNetCore.Mvc;
 using seibuDatabase.Services;
 using seibuDatabase.Models;
 
-
 namespace Fukuinct2025.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly FirebaseService _firebaseService;
 
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
-    }
-
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-
-    [HttpPost]
-    public IActionResult Post(string text)
-    {
-        this.ViewData["YourInput"] = text;
-        return View(nameof(Index));
-
-    }
-
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
-
-        private readonly FirebaseService _firebaseService;
-
-    public HomeController()
-    {
         _firebaseService = new FirebaseService();
     }
 
@@ -49,5 +22,16 @@ public class HomeController : Controller
         return View(messages);
     }
 
-}
+    [HttpPost]
+    public IActionResult Post(string text)
+    {
+        this.ViewData["YourInput"] = text;
+        return View(nameof(Index));
+    }
 
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
