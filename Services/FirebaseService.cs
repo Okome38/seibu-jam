bd3c429140fe9818353d198e148db1a7c6fa222c
Okome38 <ayato.momochan@gmail.com>
2025-05-28 15:31:12 +0900

    Update FirebaseService.cs

--- Services/FirebaseService.cs
+++ Services/FirebaseService.cs
@@ -29,5 +29,72 @@ namespace seibuDatabase.Services
 
             return messages.Select(x => x.Object).ToList();
         }
+
+        // 丼カウンターを取得
+        public async Task<DonCounter> GetDonCounter()
+        {
+            try
+            {
+                var counter = await _firebase
+                    .Child("donCounter")
+                    .OnceSingleAsync<DonCounter>();
+                
+                return counter ?? new DonCounter 
+                { 
+                    donCount = 0, 
+                    nonDonCount = 0, 
+                    lastUpdated = DateTime.UtcNow,
+                    lastUpdatedBy = "system"
+                };
+            }
+            catch
+            {
+                return new DonCounter 
+                { 
+                    donCount = 0, 
+                    nonDonCount = 0, 
+                    lastUpdated = DateTime.UtcNow,
+                    lastUpdatedBy = "system"
+                };
+            }
+        }
+
+        // 丼カウンターを更新
+        public async Task UpdateDonCounter(bool isDon, string updatedBy)
+        {
+            var currentCounter = await GetDonCounter();
+            
+            if (isDon)
+            {
+                currentCounter.donCount++;
+            }
+            else
+            {
+                currentCounter.nonDonCount++;
+            }
+            
+            currentCounter.lastUpdated = DateTime.UtcNow;
+            currentCounter.lastUpdatedBy = updatedBy;
+
+            await _firebase
+                .Child("donCounter")
+                .PutAsync(currentCounter);
+        }
+
+        // カウンターをリセット
+        public async Task ResetDonCounter(string resetBy)
+        {
+            var resetCounter = new DonCounter
+            {
+                donCount = 0,
+                nonDonCount = 0,
+                lastUpdated = DateTime.UtcNow,
+                lastUpdatedBy = resetBy
+            };
+
+            await _firebase
+                .Child("donCounter")
+                .PutAsync(resetCounter);
+        }
     }
 }
