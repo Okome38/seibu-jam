7fb2f5db6ab776a317a394e2b595f6425a139984
Okome38 <ayato.momochan@gmail.com>
2025-05-28 22:19:58 +0900

    Update FirebaseService.cs

--- Services/FirebaseService.cs
+++ Services/FirebaseService.cs
@@ -1,23 +1,53 @@
 using Firebase.Database;
+using Firebase.Auth;
 using Firebase.Database.Query;
 using seibuDatabase.Models;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
 
 namespace seibuDatabase.Services
 {
     public class FirebaseService
     {
         private readonly FirebaseClient _firebase;
+        private readonly FirebaseAuthLink _auth;
+        private readonly IConfiguration _configuration;
 
-        public FirebaseService()
+        public FirebaseService(IConfiguration configuration)
         {
-            _firebase = new FirebaseClient("https://seibudatabase-default-rtdb.firebaseio.com/");
+            _configuration = configuration;
+            
+            // Firebase設定を初期化
+            var authProvider = new FirebaseAuthProvider(
+                new FirebaseConfig(_configuration["Firebase:ApiKey"]));
+            
+            // 認証を行う（Email/Password方式）
+            _auth = authProvider.SignInWithEmailAndPasswordAsync(
+                _configuration["Firebase:AuthEmail"],
+                _configuration["Firebase:AuthPassword"]).Result;
+
+            // 認証付きのFirebaseClientを初期化
+            _firebase = new FirebaseClient(
+                _configuration["Firebase:DatabaseUrl"],
+                new FirebaseOptions
+                {
+                    AuthTokenAsyncFactory = () => Task.FromResult(_auth.FirebaseToken)
+                });
         }
 
         public async Task AddMessage(string name, string message)
         {
+            // 入力値のサニタイズ（XSS対策）
+            var sanitizedName = System.Web.HttpUtility.HtmlEncode(name);
+            var sanitizedMessage = System.Web.HttpUtility.HtmlEncode(message);
+
             await _firebase
               .Child("messages")
-              .PostAsync(new { name = name, message = message, timestamp = DateTime.UtcNow });
+              .PostAsync(new { 
+                  name = sanitizedName, 
+                  message = sanitizedMessage, 
+                  timestamp = DateTime.UtcNow 
+              });
         }
 
         public async Task<List<Message>> GetMessages()
@@ -30,7 +60,6 @@ namespace seibuDatabase.Services
             return messages.Select(x => x.Object).ToList();
         }
 
-        // 丼カウンターを取得
         public async Task<DonCounter> GetDonCounter()
         {
             try
@@ -59,9 +88,11 @@ namespace seibuDatabase.Services
             }
         }
 
-        // 丼カウンターを更新
         public async Task UpdateDonCounter(bool isDon, string updatedBy)
         {
+            // 更新者のサニタイズ
+            var sanitizedUpdatedBy = System.Web.HttpUtility.HtmlEncode(updatedBy);
+
             var currentCounter = await GetDonCounter();
             
             if (isDon)
@@ -74,22 +105,24 @@ namespace seibuDatabase.Services
             }
             
             currentCounter.lastUpdated = DateTime.UtcNow;
-            currentCounter.lastUpdatedBy = updatedBy;
+            currentCounter.lastUpdatedBy = sanitizedUpdatedBy;
 
             await _firebase
                 .Child("donCounter")
                 .PutAsync(currentCounter);
         }
 
-        // カウンターをリセット
         public async Task ResetDonCounter(string resetBy)
         {
+            // リセット実行者のサニタイズ
+            var sanitizedResetBy = System.Web.HttpUtility.HtmlEncode(resetBy);
+
             var resetCounter = new DonCounter
             {
                 donCount = 0,
                 nonDonCount = 0,
                 lastUpdated = DateTime.UtcNow,
-                lastUpdatedBy = resetBy
+                lastUpdatedBy = sanitizedResetBy
             };
 
             await _firebase
