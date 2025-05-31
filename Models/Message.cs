5661423458c6fb27cfd47f7c217d18eb7087b2a9
Okome38 <ayato.momochan@gmail.com>
2025-05-31 20:33:40 +0900

    Update Message.cs

--- Models/Message.cs
+++ Models/Message.cs
@@ -5,5 +5,8 @@ namespace seibuDatabase.Models
         public string name { get; set; }
         public string message { get; set; }
         public DateTime timestamp { get; set; }
+        public int likeCount { get; set; } = 0;
+        public int dislikeCount { get; set; } = 0;
+        public Dictionary<string, string> reactions { get; set; } = new Dictionary<string, string>();
     }
 }
